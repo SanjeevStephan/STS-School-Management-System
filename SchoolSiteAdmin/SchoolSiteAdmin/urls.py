116b5cdfbb966f087edcf4266e76d417b638eae7
Sanjeev Stephan Murmu <samstephan164@gmail.com>
2025-08-22 05:38:26 +0530

    recent changes

--- SchoolSiteAdmin/SchoolSiteAdmin/urls.py
+++ SchoolSiteAdmin/SchoolSiteAdmin/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('navigationapp.urls')),
-    path('', include('studentsapp.urls'))
+    path('', include('studentsapp.urls')),
+    path('', include('resultapp.urls'))
 ]
